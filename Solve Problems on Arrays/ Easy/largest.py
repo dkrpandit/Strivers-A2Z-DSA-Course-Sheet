c13c49856396ad6bee08caa4efa72d2d940bdc94
Dharmendra Pandit <99905700+dkrpandit@users.noreply.github.com>
2024-06-11 06:25:18 +0530

    Largest Element in Array
    Largest Element in Array

--- Solve Problems on Arrays/ Easy/largest.py
+++ Solve Problems on Arrays/ Easy/largest.py
@@ -1,3 +1,7 @@
 def largest( arr, n):
-    arr.sort()
-    return arr[n-1]
+    # arr.sort()
+    # return arr[n-1]
+    maxi=0
+    for i in range(n):
+        maxi = max(maxi,arr[i])
+    return maxi
